de2c45cf81a9f936fe9a2ff123f4a1a2be86791e
Developer <Developer@DEV>
2025-06-17 17:02:51 +0300

    77. Apinin Consume Edilmesi

--- Infrastructure/UdemyCarBook.Persistence/Repositories/CarRepositories/CarRepository.cs
+++ Infrastructure/UdemyCarBook.Persistence/Repositories/CarRepositories/CarRepository.cs
@@ -29,6 +29,9 @@ namespace UdemyCarBook.Persistence.Repositories.CarRepositories
             var values = _context.Cars.Include(x => x.Brand).ToList();
             return values;
         }
+
+      
+
         public List<Car> GetLast5CarsWithBrands()
         {
             var values = _context.Cars.Include(x => x.Brand).OrderByDescending(x => x.CarID).Take(5).ToList();

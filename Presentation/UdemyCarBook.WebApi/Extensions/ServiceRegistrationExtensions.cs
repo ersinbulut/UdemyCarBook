016c69e24dad9d174b59435a1c9018039e991b3a
Developer <Developer@DEV>
2025-07-10 10:54:21 +0300

    313. Final Dersi

---
+++ Presentation/UdemyCarBook.WebApi/Extensions/ServiceRegistrationExtensions.cs
@@ -0,0 +1,112 @@
+﻿using UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers;
+using UdemyCarBook.Application.Features.CQRS.Handlers.BannerHandlers;
+using UdemyCarBook.Application.Features.CQRS.Handlers.BrandHandlers;
+using UdemyCarBook.Application.Features.CQRS.Handlers.CarHandlers;
+using UdemyCarBook.Application.Features.CQRS.Handlers.CategoryHandlers;
+using UdemyCarBook.Application.Features.CQRS.Handlers.ContactHandlers;
+using UdemyCarBook.Application.Features.RepositoryPattern;
+using UdemyCarBook.Application.Interfaces.BlogInterfaces;
+using UdemyCarBook.Application.Interfaces.CarDescriptionInterfaces;
+using UdemyCarBook.Application.Interfaces.CarFeatureInterfaces;
+using UdemyCarBook.Application.Interfaces.CarInterfaces;
+using UdemyCarBook.Application.Interfaces.CarPricingInterfaces;
+using UdemyCarBook.Application.Interfaces.RentACarInterfaces;
+using UdemyCarBook.Application.Interfaces.ReviewInterfaces;
+using UdemyCarBook.Application.Interfaces.StatisticsInterfaces;
+using UdemyCarBook.Application.Interfaces.TagCloudInterfaces;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Persistence.Context;
+using UdemyCarBook.Persistence.Repositories.BlogRepositories;
+using UdemyCarBook.Persistence.Repositories.CarDescriptionRepositories;
+using UdemyCarBook.Persistence.Repositories.CarFeatureRepositories;
+using UdemyCarBook.Persistence.Repositories.CarPricingRepositories;
+using UdemyCarBook.Persistence.Repositories.CarRepositories;
+using UdemyCarBook.Persistence.Repositories.CommentRepositories;
+using UdemyCarBook.Persistence.Repositories.RentACarRepositories;
+using UdemyCarBook.Persistence.Repositories.ReviewRepositories;
+using UdemyCarBook.Persistence.Repositories.StatisticsRepositories;
+using UdemyCarBook.Persistence.Repositories.TagCloudRepositories;
+using UdemyCarBook.Persistence.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using UdemyCarBook.Application.Tools;
+
+namespace UdemyCarBook.WebApi.Extensions
+{
+    public static class ServiceRegistrationExtensions
+    {
+        public static IServiceCollection AddProjectServices(this IServiceCollection services)
+        {
+            services.AddScoped<CarBookContext>();
+
+            //Repositories
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped(typeof(ICarRepository), typeof(CarRepository));
+            services.AddScoped(typeof(IBlogRepository), typeof(BlogRepository));
+            services.AddScoped(typeof(ICarPricingRepository), typeof(CarPricingRepository));
+            services.AddScoped(typeof(ITagCloudRepository), typeof(TagCloudRepository));
+            services.AddScoped(typeof(IGenericRepository<>), typeof(CommentRepository<>));
+            services.AddScoped(typeof(IStatisticsRepository), typeof(StatisticsRepository));
+            services.AddScoped(typeof(IRentACarRepository), typeof(RentACarRepository));
+            services.AddScoped(typeof(ICarFeatureRepository), typeof(CarFeatureRepository));
+            services.AddScoped(typeof(ICarDescriptionRepository), typeof(CarDescriptionRepository));
+            services.AddScoped(typeof(IReviewRepository), typeof(ReviewRepository));
+
+            services.AddScoped<GetAboutQueryHandler>();
+            services.AddScoped<GetAboutByIdQueryHandler>();
+            services.AddScoped<CreateAboutCommandHandler>();
+            services.AddScoped<UpdateAboutCommandHandler>();
+            services.AddScoped<RemoveAboutCommandHandler>();
+
+            services.AddScoped<GetBannerQueryHandler>();
+            services.AddScoped<GetBannerByIdQueryHandler>();
+            services.AddScoped<CreateBannerCommandHandler>();
+            services.AddScoped<UpdateBannerCommandHandler>();
+            services.AddScoped<RemoveBannerCommandHandler>();
+
+            services.AddScoped<GetBrandQueryHandler>();
+            services.AddScoped<GetBrandByIdQueryHandler>();
+            services.AddScoped<CreateBrandCommandHandler>();
+            services.AddScoped<UpdateBrandCommandHandler>();
+            services.AddScoped<RemoveBrandCommandHandler>();
+
+            services.AddScoped<GetCarQueryHandler>();
+            services.AddScoped<GetCarByIdQueryHandler>();
+            services.AddScoped<CreateCarCommandHandler>();
+            services.AddScoped<UpdateCarCommandHandler>();
+            services.AddScoped<RemoveCarCommandHandler>();
+            services.AddScoped<GetCarWithBrandQueryHandler>();
+            services.AddScoped<GetLast5CarsWithBrandQueryHandler>();
+
+            services.AddScoped<GetCategoryQueryHandler>();
+            services.AddScoped<GetCategoryByIdQueryHandler>();
+            services.AddScoped<CreateCategoryCommandHandler>();
+            services.AddScoped<UpdateCategoryCommandHandler>();
+            services.AddScoped<RemoveCategoryCommandHandler>();
+
+            services.AddScoped<GetContactQueryHandler>();
+            services.AddScoped<GetContactByIdQueryHandler>();
+            services.AddScoped<CreateContactCommandHandler>();
+            services.AddScoped<UpdateContactCommandHandler>();
+            services.AddScoped<RemoveContactCommandHandler>();
+
+            //jwt token tanımlanması
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.RequireHttpsMetadata = false;
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidAudience = JwtTokenDefaults.ValidAudience,
+                    ValidIssuer = JwtTokenDefaults.ValidIssuer,
+                    ClockSkew = TimeSpan.Zero,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtTokenDefaults.Key)),
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true
+                };
+            });
+            return services;
+        }
+
+    }
+}
--- Presentation/UdemyCarBook.WebApi/Program.cs
+++ Presentation/UdemyCarBook.WebApi/Program.cs
@@ -1,114 +1,33 @@
 using System.Reflection;
-using System.Text;
 using FluentValidation.AspNetCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers;
-using UdemyCarBook.Application.Features.CQRS.Handlers.BannerHandlers;
-using UdemyCarBook.Application.Features.CQRS.Handlers.BrandHandlers;
-using UdemyCarBook.Application.Features.CQRS.Handlers.CarHandlers;
-using UdemyCarBook.Application.Features.CQRS.Handlers.CategoryHandlers;
-using UdemyCarBook.Application.Features.CQRS.Handlers.ContactHandlers;
-using UdemyCarBook.Application.Features.RepositoryPattern;
-using UdemyCarBook.Application.Interfaces;
-using UdemyCarBook.Application.Interfaces.BlogInterfaces;
-using UdemyCarBook.Application.Interfaces.CarDescriptionInterfaces;
-using UdemyCarBook.Application.Interfaces.CarFeatureInterfaces;
-using UdemyCarBook.Application.Interfaces.CarInterfaces;
-using UdemyCarBook.Application.Interfaces.CarPricingInterfaces;
-using UdemyCarBook.Application.Interfaces.RentACarInterfaces;
-using UdemyCarBook.Application.Interfaces.ReviewInterfaces;
-using UdemyCarBook.Application.Interfaces.StatisticsInterfaces;
-using UdemyCarBook.Application.Interfaces.TagCloudInterfaces;
 using UdemyCarBook.Application.Services;
-using UdemyCarBook.Application.Tools;
-using UdemyCarBook.Persistence.Context;
-using UdemyCarBook.Persistence.Repositories;
-using UdemyCarBook.Persistence.Repositories.BlogRepositories;
-using UdemyCarBook.Persistence.Repositories.CarDescriptionRepositories;
-using UdemyCarBook.Persistence.Repositories.CarFeatureRepositories;
-using UdemyCarBook.Persistence.Repositories.CarPricingRepositories;
-using UdemyCarBook.Persistence.Repositories.CarRepositories;
-using UdemyCarBook.Persistence.Repositories.CommentRepositories;
-using UdemyCarBook.Persistence.Repositories.RentACarRepositories;
-using UdemyCarBook.Persistence.Repositories.ReviewRepositories;
-using UdemyCarBook.Persistence.Repositories.StatisticsRepositories;
-using UdemyCarBook.Persistence.Repositories.TagCloudRepositories;
+using UdemyCarBook.WebApi.Extensions;
+using UdemyCarBook.WebApi.Hubs;
+using Microsoft.AspNetCore.SignalR;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-//jwt token tanýmlanmasý
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+builder.Services.AddHttpClient();
+
+builder.Services.AddCors(opt =>
 {
-    opt.RequireHttpsMetadata = false;
-    opt.TokenValidationParameters = new TokenValidationParameters
+    opt.AddPolicy("CorsPolicy", builder =>
     {
-        ValidAudience = JwtTokenDefaults.ValidAudience,
-        ValidIssuer = JwtTokenDefaults.ValidIssuer,
-        ClockSkew = TimeSpan.Zero,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtTokenDefaults.Key)),
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true
-    };
+        builder.AllowAnyHeader()
+        .AllowAnyMethod()
+        .SetIsOriginAllowed((host) => true)
+        .AllowCredentials();
+    });
 });
+builder.Services.AddSignalR();
 
 // Swagger servislerini ekle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Add services to the container.
-builder.Services.AddScoped<CarBookContext>();
-builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-builder.Services.AddScoped(typeof(ICarRepository), typeof(CarRepository));
-builder.Services.AddScoped(typeof(IBlogRepository), typeof(BlogRepository));
-builder.Services.AddScoped(typeof(ICarPricingRepository), typeof(CarPricingRepository));
-builder.Services.AddScoped(typeof(ITagCloudRepository), typeof(TagCloudRepository));
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(CommentRepository<>));
-builder.Services.AddScoped(typeof(IStatisticsRepository), typeof(StatisticsRepository));
-builder.Services.AddScoped(typeof(IRentACarRepository), typeof(RentACarRepository));
-builder.Services.AddScoped(typeof(ICarFeatureRepository), typeof(CarFeatureRepository));
-builder.Services.AddScoped(typeof(ICarDescriptionRepository), typeof(CarDescriptionRepository));
-builder.Services.AddScoped(typeof(IReviewRepository), typeof(ReviewRepository));
-
-builder.Services.AddScoped<GetAboutQueryHandler>();
-builder.Services.AddScoped<GetAboutByIdQueryHandler>();
-builder.Services.AddScoped<CreateAboutCommandHandler>();
-builder.Services.AddScoped<UpdateAboutCommandHandler>();
-builder.Services.AddScoped<RemoveAboutCommandHandler>();
-
-builder.Services.AddScoped<GetBannerQueryHandler>();
-builder.Services.AddScoped<GetBannerByIdQueryHandler>();
-builder.Services.AddScoped<CreateBannerCommandHandler>();
-builder.Services.AddScoped<UpdateBannerCommandHandler>();
-builder.Services.AddScoped<RemoveBannerCommandHandler>();
-
-builder.Services.AddScoped<GetBrandQueryHandler>();
-builder.Services.AddScoped<GetBrandByIdQueryHandler>();
-builder.Services.AddScoped<CreateBrandCommandHandler>();
-builder.Services.AddScoped<UpdateBrandCommandHandler>();
-builder.Services.AddScoped<RemoveBrandCommandHandler>();
-
-builder.Services.AddScoped<GetCarQueryHandler>();
-builder.Services.AddScoped<GetCarByIdQueryHandler>();
-builder.Services.AddScoped<CreateCarCommandHandler>();
-builder.Services.AddScoped<UpdateCarCommandHandler>();
-builder.Services.AddScoped<RemoveCarCommandHandler>();
-builder.Services.AddScoped<GetCarWithBrandQueryHandler>();
-builder.Services.AddScoped<GetLast5CarsWithBrandQueryHandler>();
-
-builder.Services.AddScoped<GetCategoryQueryHandler>();
-builder.Services.AddScoped<GetCategoryByIdQueryHandler>();
-builder.Services.AddScoped<CreateCategoryCommandHandler>();
-builder.Services.AddScoped<UpdateCategoryCommandHandler>();
-builder.Services.AddScoped<RemoveCategoryCommandHandler>();
-
-builder.Services.AddScoped<GetContactQueryHandler>();
-builder.Services.AddScoped<GetContactByIdQueryHandler>();
-builder.Services.AddScoped<CreateContactCommandHandler>();
-builder.Services.AddScoped<UpdateContactCommandHandler>();
-builder.Services.AddScoped<RemoveContactCommandHandler>();
+builder.Services.AddProjectServices();
 
 builder.Services.AddApplicationService(builder.Configuration);
 
@@ -130,7 +49,6 @@ builder.Services.AddSwaggerGen(options =>
     });
 });
 
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -141,7 +59,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
@@ -150,5 +68,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<CarHub>("/carhub");
 
 app.Run();
